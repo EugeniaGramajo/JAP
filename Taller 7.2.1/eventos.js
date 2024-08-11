571999dc86dd1158c2290d2ee666d2ba0ce49c30
EugeniaGramajo <gramajoeugenia@gmail.com>
2024-08-10 22:01:49 -0300

    solucion

--- Taller 7.2.1/eventos.js
+++ Taller 7.2.1/eventos.js
@@ -1,5 +1,13 @@
 const div = document.getElementById("button-container")
+const button = document.getElementById("button")
 
-div.addEventListener("click",()=>{
-    alert("Hola! Soy el div")
+
+div.addEventListener("click",(e)=>{
+    if(e.target===button){
+            alert('Hola!')
+    }
+    else {
+         alert("Hola! Soy el div")   
+    }
+
 })
